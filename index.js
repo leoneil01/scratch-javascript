b464c7ece8ecc921bc813fc5ff27adcdaf584778
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-09 09:03:21 +0800

    while loop

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello, world') //first javascript program yeheeeyy :)
+// console.log('hello, world') //first javascript program yeheeeyy :)
+
+const todos = [
+    {
+        id: 1,
+        text: 'Drink water',
+        isCompleted: true
+    },
+
+    {
+        id: 2,
+        text: 'Water the plant',
+        isCompleted: true
+    },
+
+    {
+        id: 3,
+        text: 'Check emails',
+        isCompleted: false
+    }
+];
+
+let i = 0;
+while(i < todos.length) {
+    console.log(`while loop: ${todos[i].text}`);
+    i++;
+}
