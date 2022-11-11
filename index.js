3a8838ddeb755b118fc0f4c2161b2bf405998089
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-11 21:21:29 +0800

    multiple conditions

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-// console.log('hello, world') //first javascript program yeheeeyy :)
+const x = 10
+const y = 15;
 
-const todos = [
-    {
-        id: 1,
-        text: 'Drink water',
-        isCompleted: true
-    },
+if(x > 8 || y > 10) {
+    console.log('x is greater than 8 or y is greater than 10')
+}
 
-    {
-        id: 2,
-        text: 'Water the plant',
-        isCompleted: true
-    },
-
-    {
-        id: 3,
-        text: 'Check emails',
-        isCompleted: false
-    }
-];
-
-// filter
-const todoCompleted = todos.filter(function(todo) {
-    return todo.isCompleted === true;
-}).map(function(todo){
-    return todo.text;   // filtered true but printing out the text only
-});
-console.log(todoCompleted);
+if(x > 8 && y > 10) {
+    console.log('x is greater than 8 and y is greater than 10')
+}
