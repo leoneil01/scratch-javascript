1e3034273db824bbbd6ab9e507a13e86a49bc0a4
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-12 21:20:54 +0800

    function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function addNumbers(num1, num2) {
+function addNumber1(num1, num2) {
     console.log(num1 + num2);
 }
+addNumber1(4,5);
 
-addNumbers(8, 9);
+function addNumber2(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumber2(8, 9));
