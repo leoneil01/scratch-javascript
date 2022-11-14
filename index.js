19f90578682692bd70dfac248b82164c1410b92e
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-14 17:05:20 +0800

    methods

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 function Person(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.dob = dob; // date of birth
+    this.dob = new Date(dob); // date of birth
+    
+    // Methods
+    this.getBirthYear = function() {
+    	return this.dob.getFullYear();
+    }
+    this.getFullName = function() {
+    	return `${this.firstName} ${this.lastName}`;
+    }
 }
 
 // Instantiate object
 const person1 = new Person( 'Leoneil', 'Odrunia', '6-21-2001' );
 const person2 = new Person( 'Mike', 'Ross', '12-24-1989' );
 
-console.log(person1);
-console.log(person2.dob); //getting the date of birth
+console.log(person2.getBirthYear());
+console.log(person1.getFullName());
